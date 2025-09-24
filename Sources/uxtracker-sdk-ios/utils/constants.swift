da349c417907d739fadb952f2be9f6c12eeabc05
Jorge Luis Zaragoza Barreras <jorgezb.contact@gmail.com>
2025-09-23 19:58:16 -0700

    URL Base changed

--- Sources/uxtracker-sdk-ios/utils/constants.swift
+++ Sources/uxtracker-sdk-ios/utils/constants.swift
@@ -10,5 +10,5 @@ import Foundation
 public class Constants {
     public static let SDK_VERSION = "1.0.0"
     public static let SDK_NAME = "uxtracker-sdk-ios"
-    public static let URL_BASE = URL(string: "http://192.168.100.58:8080")
+    public static let URL_BASE = URL(string: "http://192.168.100.70:8080")
 }
